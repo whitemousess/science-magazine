b2b901c575334497585aa80c3d93a3d24325f423
chuotbachweb <chuotsekiro@gmail.com>
2024-05-23 22:08:38 +0700

    handle download app header

--- src/layouts/components/Header/Header.jsx
+++ src/layouts/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import routes from "~/config/routes";
-import { MdBiotech } from "react-icons/md";
-
-import Menu from "./Menu";
-import { useContext } from "react";
-import { AuthContext } from "~/shared/AuthProvider";
 import { IoPersonAddOutline } from "react-icons/io5";
 import { CiLogin } from "react-icons/ci";
+import { useContext } from "react";
+
+import Menu from "./Menu";
+import { AuthContext } from "~/shared/AuthProvider";
+import QRApp from "~/assets/QRApp.png";
+import { FaAppStoreIos, FaGooglePlay } from "react-icons/fa";
 
 function Header() {
   const { token, role } = useContext(AuthContext);
   const navigate = useNavigate();
+  const driveLink =
+    "https://drive.google.com/file/d/1Am_xpqDH4hdpyCKWlLiFiBvePy6ikO-t/view?usp=drive_link";
 
   const MENU_HEADER = [
     {
@@ -40,38 +43,81 @@ function Header() {
     },
   ];
 
+  const handleClick = () => {
+    const isAndroid = /Android/i.test(navigator.userAgent);
+    const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    if (isAndroid || isIOS) {
+      window.location.href = `https://drive.google.com/file/d/1Am_xpqDH4hdpyCKWlLiFiBvePy6ikO-t/view?usp=drive_link`;
+    } else {
+      window.open(driveLink, "_blank");
+    }
+  };
+
   return (
     <header
       className={`py-4 bg-primary  md:px-10 min-w-full flex md:justify-between justify-center z-50 items-center fixed`}
     >
       <div className="flex ">
-        {role !== 0 &&
-          MENU_HEADER.map((item) => (
-            <div className="relative group" key={item.title}>
-              <Link
-                to={item.link}
-                className={`p-2 hover:underline text-white ${
-                  location.pathname === item.link && "underline"
-                }`}
-              >
-                {item.title}
-              </Link>
+        {role !== 0 && (
+          <>
+            {MENU_HEADER.map((item) => (
+              <div className="relative group" key={item.title}>
+                <Link
+                  to={item.link}
+                  className={`p-2 hover:underline text-white ${
+                    location.pathname === item.link && "underline"
+                  }`}
+                >
+                  {item.title}
+                </Link>
 
-              {item.arrayMenu && (
-                <div className="absolute rounded-lg overflow-hidden w-[170px] shadow bg-white hidden group-hover:block">
-                  {item.arrayMenu.map((array) => (
-                    <button
-                      onClick={() => navigate(array.link)}
-                      key={array.title}
-                      className="px-4 py-2 w-full text-left hover:bg-gray-100 cursor-pointer"
-                    >
-                      {array.title}
-                    </button>
-                  ))}
+                {item.arrayMenu && (
+                  <div className="absolute rounded-lg overflow-hidden w-[170px] shadow bg-white hidden group-hover:block">
+                    {item.arrayMenu.map((array) => (
+                      <button
+                        onClick={() => navigate(array.link)}
+                        key={array.title}
+                        className="px-4 py-2 w-full text-left hover:bg-gray-100 cursor-pointer"
+                      >
+                        {array.title}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+
+            <div className="group relative">
+              <button className="px-2 hover:underline text-white">
+                Tải ứng dụng
+              </button>
+
+              <div className="hidden group-hover:block bg-white absolute right-0 p-6  rounded-lg shadow-xl">
+                <div className="w-[320px] flex flex-col items-center">
+                  <img
+                    src={QRApp}
+                    className="w-[200px] h-[200px] object-cover"
+                  />
                 </div>
-              )}
+
+                <div className="flex justify-evenly mt-4">
+                  <button className="flex justify-center items-center pr-4 border-r">
+                    <FaAppStoreIos className="mr-1" size={32} />
+                    Coming soon
+                  </button>
+                  <button
+                    onClick={handleClick}
+                    className="flex justify-center items-center"
+                  >
+                    <FaGooglePlay className="mr-1" size={32} /> Download file
+                    APK
+                  </button>
+                </div>
+              </div>
             </div>
-          ))}
+          </>
+        )}
       </div>
 
       {token ? (
@@ -95,12 +141,12 @@ function Header() {
 
           <div className="fixed flex md:hidden bottom-0 z-50 bg-primary w-full">
             <CiLogin
-            size={50}
+              size={50}
               onClick={() => navigate(routes.login)}
               className="w-1/2 py-3 font-bold text-white hover:bg-blue-600"
             />
             <IoPersonAddOutline
-            size={50}
+              size={50}
               onClick={() => navigate(routes.register)}
               className="w-1/2 py-3 font-bold text-white hover:bg-blue-600"
             />
